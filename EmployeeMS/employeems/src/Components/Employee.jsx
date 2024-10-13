db44a1dae9eb3a180d0c77bbdd3a4a5ab9a1aed7
Le Quoc Bao <145678921+BaoWhiteHat@users.noreply.github.com>
2024-10-13 15:20:18 +0700

    Employee

--- EmployeeMS/employeems/src/Components/Employee.jsx
+++ EmployeeMS/employeems/src/Components/Employee.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Employee = () => {
   return (
-    <div>
-      Employee
+    <div className="px-5 mt-3">
+      <div className="d-flex justify-content-center">
+        <h3>Employee List</h3>
+      </div>
+      <Link to="/dashboard/add_employee" className="btn btn-success">
+        Add Employee
+      </Link>
+      <div className="mt-3"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Employee
+export default Employee;
