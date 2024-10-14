a7abae298e94980e45662607ef9ea5ec62072a8b
Le Quoc Bao <145678921+BaoWhiteHat@users.noreply.github.com>
2024-10-14 17:55:11 +0700

    Finisheddddd

--- EmployeeMS/employeems/src/Components/Dashboard.jsx
+++ EmployeeMS/employeems/src/Components/Dashboard.jsx
@@ -4,14 +4,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import axios from "axios";
 
 const Dashboard = () => {
-  const navigate = useNavigate()
+  const anvigate = useNavigate()
   axios.defaults.withCredentials = true
   const handleLogout = () => {
     axios.get('http://localhost:3000/auth/logout')
     .then(result => {
       if(result.data.Status) { 
         localStorage.removeItem("valid")
-        navigate('/')
+        anvigate('/')
       }
     })
   }
@@ -20,21 +20,18 @@ const Dashboard = () => {
       <div className="row flex-nowrap">
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-
             <Link
               to="/dashboard"
               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
             >
               <span className="fs-5 fw-bolder d-none d-sm-inline">
-                Human Resources
+                Code With Yousof
               </span>
             </Link>
-            
             <ul
               className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
               id="menu"
             >
-
               <li className="w-100">
                 <Link
                   to="/dashboard"
@@ -44,7 +41,6 @@ const Dashboard = () => {
                   <span className="ms-2 d-none d-sm-inline">Dashboard</span>
                 </Link>
               </li>
-
               <li className="w-100">
                 <Link
                   to="/dashboard/employee"
@@ -55,9 +51,7 @@ const Dashboard = () => {
                     Manage Employees
                   </span>
                 </Link>
-                
               </li>
-
               <li className="w-100">
                 <Link
                   to="/dashboard/category"
@@ -67,7 +61,6 @@ const Dashboard = () => {
                   <span className="ms-2 d-none d-sm-inline">Category</span>
                 </Link>
               </li>
-
               <li className="w-100">
                 <Link
                   to="/dashboard/profile"
@@ -77,7 +70,6 @@ const Dashboard = () => {
                   <span className="ms-2 d-none d-sm-inline">Profile</span>
                 </Link>
               </li>
-
               <li className="w-100" onClick={handleLogout}>
               <Link
                   className="nav-link px-0 align-middle text-white"
@@ -86,13 +78,12 @@ const Dashboard = () => {
                   <span className="ms-2 d-none d-sm-inline">Logout</span>
                 </Link>
               </li>
-
             </ul>
           </div>
         </div>
         <div className="col p-0 m-0">
             <div className="p-2 d-flex justify-content-center shadow">
-                <h4>Employee Management System</h4>
+                <h4>Emoployee Management System</h4>
             </div>
             <Outlet />
         </div>
